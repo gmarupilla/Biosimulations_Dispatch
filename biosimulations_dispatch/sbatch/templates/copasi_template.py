706219aae1cd0fa01686ccfd01e437107c69d7ed
gmarupilla <marupilla@uchc.edu>
2020-02-07 16:15:45 -0500

    Update copasi template

--- biosimulations_dispatch/sbatch/templates/copasi_template.py
+++ biosimulations_dispatch/sbatch/templates/copasi_template.py
@@ -39,7 +39,7 @@ env
 localSingularityImage="/home/CAM/crbmapi/copasi_latest.img"
 
 # TODO: Update COPASI SBATCH Template
-command="SINGULARITYENV_AUTH0_CLIENT_ID=$AUTH0_CLIENT_ID SINGULARITYENV_AUTH0_CLIENT_SECRET=$AUTH0_CLIENT_SECRET SINGULARITYENV_AUTH0_BIOSIMULATIONS_AUDIENCE=$AUTH0_BIOSIMULATIONS_AUDIENCE SINGULARITYENV_AUTH0_TOKEN_URL=$AUTH0_TOKEN_URL SINGULARITYENV_AUTH0_GRANT_TYPE=$AUTH0_GRANT_TYPE SINGULARITYENV_SIMULATION_ID={simulation_id} SINGULARITYENV_USER_SUB_ID={subscriber_id}  SINGULARITYENV_JOB_ID=$SLURM_JOB_UID   SINGULARITYENV_JOBHOOK_URL={jobhook_url} singularity run --bind $TMPDIR:/usr/local/app/copasi/simulation --pwd=/app $localSingularityImage"
+command="SINGULARITYENV_AUTH0_CLIENT_ID=$AUTH0_CLIENT_ID SINGULARITYENV_AUTH0_CLIENT_SECRET=$AUTH0_CLIENT_SECRET SINGULARITYENV_AUTH0_BIOSIMULATIONS_AUDIENCE=$AUTH0_BIOSIMULATIONS_AUDIENCE SINGULARITYENV_AUTH0_TOKEN_URL=$AUTH0_TOKEN_URL SINGULARITYENV_AUTH0_GRANT_TYPE=$AUTH0_GRANT_TYPE SINGULARITYENV_SIMULATION_ID={simulation_id}  SINGULARITYENV_JOB_ID=$SLURM_JOB_UID   SINGULARITYENV_JOBHOOK_URL={jobhook_url} singularity run --bind $TMPDIR:/usr/local/app/copasi/simulation --pwd=/app $localSingularityImage"
 echo $command
 
 eval $command'''
@@ -48,7 +48,7 @@ eval $command'''
 class CopasiTemplate:
     def __init__(self):
         self.template_string = template_string
-        self.key_list = ['simulation_id', 'temp_dir', 'jobhook_url', 'subscriber_id']
+        self.key_list = ['simulation_id', 'temp_dir', 'jobhook_url']
 
     def fill_values(self, value_dict: dict):
         return self.template_string.format(**value_dict)
