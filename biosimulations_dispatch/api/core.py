b5cae135a8e94f7e45ae7c1add4aa6bed9e9795b
gmarupilla <marupilla@uchc.edu>
2020-03-18 15:10:12 -0400

    Dispatch API working, sample.env added

---
+++ biosimulations_dispatch/api/core.py
@@ -0,0 +1,47 @@
+import json
+import os
+from werkzeug.contrib.fixers import ProxyFix
+from flask import Flask, request, redirect, url_for, abort, jsonify
+from flask_cors import CORS, cross_origin
+from biosimulations_dispatch.config import Config
+from biosimulations_dispatch.hpc_manager import HPCManager
+
+class PrefixMiddleware:
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+
+
+app = Flask(__name__)
+cors = CORS(app)
+
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='')
+app.wsgi_app = ProxyFix(app.wsgi_app)
+@app.route('/dispatch', methods=['POST'])
+@cross_origin()
+def dispatch_to_hpc():
+    if request.method == 'POST' and request.remote_addr == Config.ALLOWED_ORIGIN:
+        try:
+            hpc_manager = HPCManager(username=Config.HPC_USER, password=Config.HPC_PASS, server=Config.HPC_HOST)
+            data = request.get_json()
+            hpc_manager.dispatch_job(
+                # TODO: parse simulator from sim spec within dispatch_job
+                    simulator = data['simSpec']['simulator'],
+                    value_dict = data['simSpec'],
+                    sedml = data['sedml'],
+                    sedml_name = data['sedmlName'],
+                    sbml = data['sbml'],
+                    temp_dir = Config.TEMP_DIR
+                )
+            return jsonify({"message": 'Simulation has been successfully dispatched to HPC'}), 200
+        except BaseException as ex:
+            return jsonify({'message': "Error occured: " + str(ex)}), 400
--- biosimulations_dispatch/config.py
+++ biosimulations_dispatch/config.py
@@ -17,3 +17,5 @@ class Config(object):
     HPC_USER = os.environ.get('HPC_USER')
     HPC_PASS = os.environ.get('HPC_PASS')
     JOBHOOK_URL = '{}/jobhook'.format(os.getenv('JOBHOOK_HOST'))
+    ALLOWED_ORIGIN = os.environ.get('ALLOWED_ORIGIN')
+    TEMP_DIR = os.environ.get('TEMP_DIR')
